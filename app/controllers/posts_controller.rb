464da213613ff3f47f057f282ab2a0183f228397
Danh Manh Nguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-06-25 21:54:54 +0700

    add CRUD Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,47 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def update
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else render 'posts/new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else render 'posts/edit'
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
