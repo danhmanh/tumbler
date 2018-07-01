1a4d8179a7961533d95fbbf7031745dc6ca68c68
Danh Manh Nguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-07-01 13:41:44 +0700

    setting up Devise & modify navbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { in: 6..20 }
+  validates :title, presence: true, length: { in: 6..50 }
   validates :content, presence: true
 
 end
