d1e79f0d37491587d221df6f7fa0841101518273
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-07-08 21:57:26 +0700

    set avatar for user and display their own posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+  Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs, user_id: Faker::Number.between(1, 3))
+end
