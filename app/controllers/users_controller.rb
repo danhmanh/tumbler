b9ee44a244520de79c49409d408debe6818cdcbf
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-07-09 11:35:28 +0700

    add Post's User pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:update]
   def show
     @user = current_user
+    @posts = current_user.posts.page(params[:page]).per(4)
   end
 
   def edit
